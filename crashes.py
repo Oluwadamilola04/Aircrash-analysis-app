77677404ad76ad0227ade7f746e229dcab7bb73b
Damilola Akinrinmade <119226082+Oluwadamilola04@users.noreply.github.com>
2024-08-30 18:54:28 +0100

    Update crashes.py

--- crashes.py
+++ crashes.py
@@ -1,7 +1,9 @@
+import os
+os.system('pip install matplotlib')
 import streamlit as st
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt 
 
 def load_data():
     file = 'aircrash.csv'
