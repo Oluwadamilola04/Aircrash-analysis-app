ab92b9344bb6286ce0c5839c9fabcbfbb9035114
Damilola Akinrinmade <119226082+Oluwadamilola04@users.noreply.github.com>
2024-08-30 18:34:00 +0100

    Update crashes.py

--- crashes.py
+++ crashes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt # type: ignore
+import matplotlib.pyplot as plt 
 
 def load_data():
     file = 'aircrash.csv'
@@ -36,13 +36,13 @@ from_year, to_year = st.slider(
 
 # display the table
 #st.dataframe(df.head(100))
-
+dfnew = df[(df['Year'] <= to_year) & (from_year <= df['Year'])]
 
 # display metrics
-Total_Fatalities = df['Total Fatalities'].sum()
-Total_Accidents = df['Aircraft'].count()
-Total_Passengers_aboard = df['Sum of Aboard'].sum()
-Casualties_ground = df['Sum of Fatalities(ground)'].sum()
+Total_Fatalities = dfnew['Total Fatalities'].sum()
+Total_Accidents = dfnew['Aircraft'].count()
+Total_Passengers_aboard = dfnew['Sum of Aboard'].sum()
+Casualties_ground = dfnew['Sum of Fatalities(ground)'].sum()
 
 st.subheader('Key Numbers')
 col1, col2, col3, col4 = st.columns(4)
@@ -55,11 +55,15 @@ col4.metric('Total ground casualties', Casualties_ground)
 # end of metrics
 ''
 ''
+
 # display the filtered table
-st.dataframe(df)
+
+
+st.dataframe(dfnew[['Crash Date','Country/Region','Aircraft Manufacturer',
+                    'Aircraft','Operator','Sum of Aboard','Total Fatalities']])
 
 # 
-dfnew = df[(df['Year'] <= to_year) & (from_year <= df['Year'])]
+
 # area chart
 try:
     st.header(f'Yearly trend of Fatalities',divider='orange')
@@ -76,6 +80,50 @@ try:
     st.header(f'Aircrash trends over the years',divider='orange')
     line1 = dfnew.groupby('Year')['Aircraft'].count()
     st.area_chart(line1,color='#ffaa00')
+except ValueError as e:
+    st.error(
+        """ Error: """ % e.reason
+    )
+
+# horizontal bar chart
+try:
+    st.header('Top 15 airlines with the most crashes',divider='orange')
+    df2 = dfnew['Operator'].value_counts().head(15).sort_values(ascending=True)
+    plt.figure(figsize=(10,4))
+    plt.barh(df2.index,df2.values,color='#ffaa00')
+
+    for i,value in enumerate(df2.values):
+       plt.annotate(str(value),xy=(value,i),
+       ha='left',va='center')
+    plt.xlabel('Airline')
+    plt.ylabel('Number of crashes')
+    
+    plt.yticks(rotation=0,ha='right')
+    plt.show()
+    
+    st.pyplot(plt)
+except ValueError as e:
+    st.error(
+        """ Error: """ % e.reason
+    )
+
+# horizontal bar chart
+try:
+    st.header(f'Aircraft Models with most number of Accidents',divider='orange')
+    df3 = dfnew['Aircraft'].value_counts().head(15).sort_values(ascending=True)
+    plt.figure(figsize=(10,4))
+    plt.barh(df3.index,df3.values,color='#ffaa00')
+
+    for i,value in enumerate(df3.values):
+     plt.annotate(str(value),xy=(value,i),
+     ha='left',va='center')
+    plt.xlabel('Aircraft')
+    plt.ylabel('Number of crashes')
+    
+    plt.yticks(rotation=0,ha='right')
+    plt.show()
+
+    st.pyplot(plt)
 except ValueError as e:
     st.error(
         """ Error: """ % e.reason
